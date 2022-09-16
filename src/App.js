9d10de63740d504fe7777b939212f11820a3912f
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-16 16:14:37 +0100

    Names input box

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='container my-5'>
+      <form>
+        <div className='my-3'>
+          <label htmlFor='input-temp' classname='form-label'>
+            Input Temperature
+          </label>
+          <input
+            id="input-temp"
+            name="input-temp"
+            className='form-control'
+          />
+        </div>
+      </form>      
     </div>
   );
 }
