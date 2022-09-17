744e0b2642c001707e0a8b051f58cf2f664fa65b
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-17 13:42:35 +0100

    Checks that Jest is working.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test("the initial input box is empty", ()=>{
+// Smoke Test - Checks that jest is working!
+//a describe block groups several tests together
+describe("check that Jest is working", () => {
+  test("that 2+2 is 4", () => {
+      expect(2+2).toBe(4); // smoke test's assertion
+  })
+});
+
+test("the initial input box is empty", () => {
   render(<App/>);
   const celsiusInput = screen.getByRole('textbox');//Gets the input box and assigns it to a variable
   expect(celsiusInput.value).toBe('');//Checks that initial input box value is an empty string
