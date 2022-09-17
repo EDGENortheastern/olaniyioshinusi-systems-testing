e2e8070f4a63e382f5cfe6421f29255fabcbd7c0
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-17 15:17:04 +0100

    Adds a failing test

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ test("the initial input box is empty", ()=>{
   render(<App/>);
   const celsiusInput = screen.getByRole('textbox');//Gets the input box and assigns it to a variable
   expect(celsiusInput.value).toBe('');//Checks that initial input box value is an empty string
+  expect(celsiusInput).toHaveStyle({backgroundColor: "beige"})
 });
 
 
