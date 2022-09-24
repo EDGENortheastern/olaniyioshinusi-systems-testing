1d15bed08a4a123684bed8d95117f7b6e831ea14
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-24 19:46:58 +0100

    Adds basic user interface with weight and height input boxes and buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  // state
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+
+  let imgSrc = ''
+
   return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor='input-temp'>
-            Input Temperature
-          </label>
-          <input
-            id="input-temp"
-            name="input-temp"
-          />
+    <div className="app">
+      <div className='container'>
+        <h1 className='center'>BMI Calculator</h1>
+        <form >
+          <div>
+            <label>Weight (kg)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (metres)</label>
+            <input value={height} onChange={(e) => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline'  type='submit'>Reload</button>
+          </div>
+        </form>
+
+        <div className='center'>
+          <h2>Your BMI is: {bmi}</h2>
+          <p>{message}</p>
         </div>
-      </form>      
+
+        <div className='img-container'>
+          <img src={imgSrc} alt=''></img>
+        </div>
+      </div>        
     </div>
   );
 }
