20bbaa1179996d8d32962a2533d60bb1458e9bf6
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-24 20:50:31 +0100

    Adds logic to application and reload functionality

--- src/App.js
+++ src/App.js
@@ -11,11 +11,37 @@ function App() {
 
   let imgSrc = ''
 
+  let calcBmi = (event) => {
+    //prevent submitting, e=event
+    event.preventDefault()
+
+    if (weight === 0 || height === 0) {
+      alert('Please enter a valid weight and height')
+    } else {
+      let bmi = (weight / (height * height))
+      setBmi(bmi.toFixed(1)) 
+
+      // Logic for message
+
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+  
+  let reload = () => {
+    window.location.reload()
+  }
+
   return (
     <div className="app">
       <div className='container'>
         <h1 className='center'>BMI Calculator</h1>
-        <form >
+        <form onSubmit={calcBmi}>
           <div>
             <label>Weight (kg)</label>
             <input value={weight} onChange={(e) => setWeight(e.target.value)} />
@@ -26,7 +52,7 @@ function App() {
           </div>
           <div>
             <button className='btn' type='submit'>Submit</button>
-            <button className='btn btn-outline'  type='submit'>Reload</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
           </div>
         </form>
 
