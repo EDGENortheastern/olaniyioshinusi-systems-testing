6ab25218f5803afe1ef4e3c37df6329e8059c3bc
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-10-03 16:54:39 +0100

    Logic test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test("the initial input box is empty", ()=>{
+  
   render(<App/>);
-  expect(celsiusInput.value).toBe('');//Checks that initial input box value is an empty string
-  //expect(celsiusInput).toHaveStyle({backgroundColor: "beige"})
+  const weightInput = screen.getByLabelText(/Weight (kg)/);
+  expect(weightInput.value).toBe('0');//Checks that initial input box value is an empty string
+  
 });
 
 
