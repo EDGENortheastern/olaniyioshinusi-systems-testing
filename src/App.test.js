359ca5dfd3fe484b27079d386c70eae4ed574cc4
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-10-03 16:04:42 +0100

    Update src/App.test.js
    Co-authored-by: jcsaenzcarrasco <jcsaenzcarrasco@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test("the initial input box is empty", ()=>{
   render(<App/>);
-  const celsiusInput = screen.getByRole('textbox');//Gets the input box and assigns it to a variable
   expect(celsiusInput.value).toBe('');//Checks that initial input box value is an empty string
   //expect(celsiusInput).toHaveStyle({backgroundColor: "beige"})
 });
