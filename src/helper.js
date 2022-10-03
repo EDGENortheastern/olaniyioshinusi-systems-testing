2e0db8776f49b410efcd58fc9fa52cbfe58ef07d
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-10-03 16:08:36 +0100

    Delete helper.js

--- src/helper.js
+++
@@ -1,8 +0,0 @@
-const convertToNum = (val) => parseInt(val);
-
-const convertToFarenheit = (celsius) => convertToNum(celsius) * (9 / 5) + 32;
-
-export {
-    convertToNum,
-    convertToFarenheit
-};
